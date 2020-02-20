61ef35601fbed4dbec33303ad9928ff4165385d9
Slick Kilmister <kilian@catsoft.ch>
2020-02-20 21:37:43 +0100

    getting ready

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
-/** @format */
+#!/usr/bin/env node
+const chalk = require( 'chalk' )
+const figlet = require( 'figlet' )
 
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+const files = require( './lib/files' )
+const github = require( './lib/github' )
+const repo = require( './lib/repo' )
 
-const files = require('./lib/files');
-const github = require('./lib/github');
-const repo = require('./lib/repo');
+console.log( chalk.blue( figlet.textSync( 'Sinit', { horizontalLayout: 'full' } ) ) )
 
-clear();
-
-console.log(chalk.yellow(figlet.textSync('Ginit', { horizontalLayout: 'full' })));
-
-if (files.directoryExists('.git')) {
-  console.log(chalk.red('Already a Git repository!'));
-  process.exit();
+if ( files.directoryExists( '.git' ) ) {
+  throw new Error( chalk.red( 'Already a Git repository!' ) )
 }
 
-const getGithubToken = async () => {
+const getGithubToken = async() => {
+
   // Fetch token from config store
-  let token = github.getStoredGithubToken();
-  if (token) {
-    return token;
+  let token = github.getStoredGithubToken()
+  if ( token ) {
+    return token
   }
 
   // No token found, use credentials to access Github account
-  token = await github.getPersonalAccessToken(); //NOTE
+  token = await github.getPersonalAccessToken() // NOTE
 
-  return token;
-};
+  return token
+}
 
-const run = async () => {
+const run = async() => {
   try {
+
     // Retrieve and Set Authentication Token
-    const token = await getGithubToken();
-    github.githubAuth(token);
+    const token = await getGithubToken()
+    github.githubAuth( token )
 
     // Create remote repository
-    const url = await repo.createRemoteRepo();
+    const url = await repo.createRemoteRepo()
 
     // Create gitignore file
-    await repo.createGitignore();
+    await repo.createGitignore()
 
     // Set up local repository and push to remote
-    await repo.setupRepo(url);
-
-    console.log(chalk.green('All done!'));
-  } catch(err) {
-    if (err) {
-      switch (err.status) {
+    await repo.setupRepo( url )
+    console.log( chalk.green( 'All done!' ) )
+  } catch ( err ) {
+    if ( err ) {
+      switch ( err.status ) {
         case 401:
-          console.log(chalk.red("Coudn't log in. Please check credentials/token."));
-          break;
+          console.log(
+            chalk.red( "Coudn't log in. Please check credentials/token." ),
+          )
+          break
         case 422:
-          console.log(chalk.red('There is already a remote repository or token with the same name.'));
-          break;
+          console.log(
+            chalk.red(
+              'There is already a remote repository or token with the same name.',
+            ),
+          )
+          break
         default:
-          console.log(chalk.red(err));
+          console.log( chalk.red( err ) )
       }
     }
   }
-};
+}
 
-run();
+run()
